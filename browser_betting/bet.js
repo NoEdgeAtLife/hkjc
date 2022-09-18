79b8cf2613713ad1f65ca2b9b22d5b13c17e61ed
cityueegef <johneinsteinwong@gmail.com>
2022-09-18 22:01:58 +0800

    Update bet.js

--- browser_betting/bet.js
+++ browser_betting/bet.js
@@ -5,15 +5,24 @@ let bet_amount = [100];
 let horse_no = [3];
 
 async function bet() {
-    document.getElementById("raceSel"+raceno).click();
+    document.getElementById("raceSel" + raceno).click();
     for (let i = 0; i < horse_no.length; i++) {
-        await setTimeout(()=>{document.getElementById("winCell"+horse_no).getElementsByTagName("span")[0].getElementsByTagName("a")[0].click();}, 1000);
+        await setTimeout(()=>{
+            document.getElementById("winCell" + horse_no).getElementsByTagName("span")[0].getElementsByTagName("a")[0].click();
+        }
+        , 1000);
         // setTimeout(()=>{document.querySelector(".rsInvCalUnitBetInput").value = 100;}, 500);
-        await setTimeout(()=>{document.getElementById("inputAmount"+i).value = bet_amount[i];
-                        CheckRefreshUnitbet(i);
-        }, 1500);
+        await setTimeout(()=>{
+            document.getElementById("inputAmount" + i).value = bet_amount[i];
+            CheckRefreshUnitbet(i);
+        }
+        , 1500);
     }
-    setTimeout(()=>{OnClickPreview();}, 1000*horse_no.length+2000);
+    setTimeout(()=>{
+        OnClickPreview();
+    }
+    , 1000 * horse_no.length + 2000);
+	console.log('Please preview your bet and confirm.');
     // setTimeout(()=>{OnClickConfirmBet();}, 2500);
 }
 
